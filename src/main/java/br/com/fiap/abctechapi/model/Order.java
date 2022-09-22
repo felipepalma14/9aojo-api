9cc981f6610f5f5beed04900befdb591d58173ac
Felipe Palma <felipe.palma@rentcars.com>
2022-09-22 18:24:50 -0400

    Camada de application

---
+++ src/main/java/br/com/fiap/abctechapi/model/Order.java
@@ -0,0 +1,43 @@
+package br.com.fiap.abctechapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "operator_id", nullable = false)
+    private Long operatorId;
+
+    @ManyToMany
+    private List<Assistance> assists;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "start_order_location_id", foreignKey = @ForeignKey(name ="FK_start_order_id"))
+    private OrderLocation startOrderLocation;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "end_order_location_id" , foreignKey = @ForeignKey(name ="FK_end_order_id"))
+    private OrderLocation endOrderLocation;
+
+
+    public boolean hasMinAssists (){
+        return assists.size() > 0;
+    }
+
+    public boolean exceedsMaxAssists () {
+        return assists.size() > 15;
+    }
+}
